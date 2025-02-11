5227afafe0b104f232242d1b191d211933522f68
erdospharmacist <careers.teicher.e@gmail.com>
2025-02-11 19:02:52 +1100

    maths functions git repo

---
+++ sum of gcd function.py
@@ -0,0 +1,15 @@
+import math
+
+def gcd_sum(n):
+    return sum(math.gcd(k, n) for k in range(1, n + 1))
+
+def find_divisors(bound):
+    results = []
+    for n in range(1, bound + 1):
+        if gcd_sum(n) % n == 0:
+            results.append(n)
+    return results
+
+bound = int(input("Enter an upper bound: "))
+divisible_numbers = find_divisors(bound)
+print(f"Numbers n up to {bound} where n divides S(n): {divisible_numbers}")
